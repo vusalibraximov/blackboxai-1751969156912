9c853fc53fad6c95219d2a9756cdddea5c5aad17
vusalibraximov <168263862+vusalibraximov@users.noreply.github.com>
2025-07-08 14:06:14 +0400

    Add app/src/main/java/com/example/turboazclone/ui/theme/Color.kt 1751969173642

---
+++ app/src/main/java/com/example/turboazclone/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.turboazclone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFFFF0000) // Red
+val md_theme_light_onPrimary = Color(0xFFFFFFFF) // White
+val md_theme_light_primaryContainer = Color(0xFFFFCDD2) // Light Red
+val md_theme_light_onPrimaryContainer = Color(0xFFB71C1C) // Dark Red
+
+val md_theme_dark_primary = Color(0xFFFF8A80) // Light Red
+val md_theme_dark_onPrimary = Color(0xFFB71C1C) // Dark Red
+val md_theme_dark_primaryContainer = Color(0xFFB71C1C) // Dark Red
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFCDD2) // Light Red
