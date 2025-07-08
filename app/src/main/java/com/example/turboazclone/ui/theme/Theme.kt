991cd21e581f4f5dd6ec235639d89cd4ef3a806f
vusalibraximov <168263862+vusalibraximov@users.noreply.github.com>
2025-07-08 14:06:13 +0400

    Add app/src/main/java/com/example/turboazclone/ui/theme/Theme.kt 1751969172245

---
+++ app/src/main/java/com/example/turboazclone/ui/theme/Theme.kt
@@ -0,0 +1,35 @@
+package com.example.turboazclone.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+)
+
+private val DarkColors = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+)
+
+@Composable
+fun TurboAzCloneTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
